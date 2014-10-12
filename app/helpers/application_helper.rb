6e05ed1e10dae12df64d88e6e6318b16d2a7cabe
jimmywalkergit <jimmy.the.walk@gmail.com>
2014-10-12 16:43:09 -0400

    working generation helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,49 @@
 module ApplicationHelper
+
+
+
+	def randomnums()
+
+
+		randomnum = rand(8**8).to_s(8)
+
+
+	return randomnum
+
+	end
+
+	
+
+	def randomletters()
+		i = 0
+		letters = "abcdefghijklmnopqrstuvwxyz"
+		randomstring = ""
+
+			while i<8 do
+				randomstring << letters[rand(26)]
+				i += 1
+			end	
+			
+
+			return randomstring
+	end
+
+	def randomletternum()
+		i=0
+		letternums = "abcdefghijklmnopqrstuvwxyz1234567890"
+		randomstring = ""
+
+		while i<8 do
+				randomstring << letternums[rand(36)]
+				i += 1
+			end	
+
+			return randomstring
+
+	end
+
+
+
+
 end
+
