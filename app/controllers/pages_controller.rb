a42f8ced99849c65a21e04862570b74e24076687
jimmywalkergit <jimmy.the.walk@gmail.com>
2014-11-03 21:55:11 -0500

    added mikes algorithm

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,15 @@ class PagesController < ApplicationController
   def stringsub
 	forminput = params['mystring']
 	genlength = params['lengthtomake']
+	mikesinput = params['mikesinput']
+
+	mikesoutput = mikesalgorithm(mikesinput)
+
+	if mikesoutput.length < 1
+mikesoutput = "Invalid Entry"
+	end
+
+	@mikesdisplay = mikesoutput
 
 mynum = String.new(genlength)
 calccheck = checknum(mynum)
@@ -28,8 +37,8 @@ mypass1 = generateletterpass(mynum)
 mypass2 = generatesecurepass(mynum)
 @lengthsecurepass = mypass2
 else
-	@lengthsecurepass = "invalid length"
-	@lengthletterpass = "invalid length"
+	@lengthsecurepass = "Invalid Length"
+	@lengthletterpass = "Invalid Length"
 end
 
 @inputlength = mynum
@@ -207,6 +216,206 @@ end
 def show
 
 end
+
+
+	def mikesalgorithm(userinput)
+	myString =  String.new(userinput)
+
+
+testString = myString.length
+puts testString
+puts myString
+ranA= "@&8*"
+ranB = "8&*"
+ranC = "(<"
+ranD = ")>}]"
+ranE = "3{["
+ranF = "45"
+ranG = "&6"
+ranH = "4#"
+ranI = "!"
+ranJ = ";"
+ranK = "("
+ranL = "</"
+ranM = "#/"
+ranN = ">^"
+ranO = "0*"
+ranP =")"
+ranQ = "0"
+ranR ="/?"
+ranS = "$5"
+ranT ="+"
+ranU = "^vV"
+ranV ="uU"
+ranW ="U2@"
+ranX = "#%*"
+ranY = "vV>"
+ranZ = ">/&"
+ranOne = "!"
+ranTwo = "+"
+ranThree = "#"
+ranFour = "#"
+ranFive = "%"
+ranSix = "^"
+ranSev = "&"
+ranEig = "*"
+ranNin = "N"
+ranZero = "Z"
+
+
+
+i=0
+
+ranNum = rand(10)
+#increments random number so that it doesn't equate to zero
+ranNum += 1
+ 
+
+      while i<myString.length do
+        #checks if random number is even or odd
+        
+        
+         #   puts ranNum
+            ranNum += 1
+         if ranNum % 2 == 0 then
+          ranNumBoo = "even"
+            #puts "even" 
+         elsif ranNum % 2 == 1 then
+          ranNumBoo = "odd"
+            #puts "odd"
+         end
+         
+         #secRanNum = ""
+         #secRanNum = ranNum + rand(2)
+         #ranNum += secRanNum
+           
+      #puts ranNum
+        if ranNumBoo == "odd" then
+            
+              if myString[i] ==  "a" then
+                randomString = ranA[rand(ranA.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "b" then
+                 randomString = ranB[rand(ranB.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "c" then
+                randomString = ranC[rand(ranC.length)]
+                 myString[i] = randomString
+             elsif myString[i] == "d" then
+                randomString = ranD[rand(ranD.length)]
+                 myString[i] = randomString
+             elsif myString[i] == "e" then
+                randomString = ranE[rand(ranE.length)]
+                 myString[i] = randomString
+             elsif myString[i] == "f" then
+                randomString = ranF[rand(ranF.length)]
+                 myString[i] = randomString
+             elsif myString[i] == "g" then
+                randomString = ranG[rand(ranG.length)]
+                 myString[i] = randomString
+             elsif myString[i] == "h" then
+                randomString = ranH[rand(ranG.length)]
+                 myString[i] = randomString
+             elsif myString[i] == "i" then
+                randomString = ranI[rand(ranI.length)]
+                 myString[i] = randomString    
+             elsif myString[i] == "j" then
+                randomString = ranJ[rand(ranJ.length)]
+                 myString[i] = randomString     
+             elsif myString[i] == "k" then
+                randomString = ranK[rand(ranK.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "l" then
+                randomString = ranL[rand(ranL.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "m" then
+                randomString = ranM[rand(ranM.length)]
+                 myString[i] = randomString  
+              elsif myString[i] == "n" then
+                randomString = ranN[rand(ranN.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "o" then
+                randomString = ranO[rand(ranO.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "p" then
+                randomString = ranP[rand(ranP.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "q" then
+                randomString = ranQ[rand(ranQ.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "r" then
+                randomString = ranR[rand(ranR.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "s" then
+                randomString = ranS[rand(ranS.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "t" then
+                randomString = ranT[rand(ranT.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "u" then
+                randomString = ranU[rand(ranU.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "v" then
+                randomString = ranV[rand(ranV.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "w" then
+                randomString = ranW[rand(ranW.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "x" then
+                randomString = ranX[rand(ranX.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "y" then
+                randomString = ranY[rand(ranY.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "z" then
+                randomString = ranZ[rand(ranZ.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "1" then
+                randomString = ranOne[rand(ranOne.length)]
+                 myString[i] = randomString    
+              elsif myString[i] == "2" then
+                randomString = ranTwo[rand(ranTwo.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "3" then
+                randomString = ranThree[rand(ranThree.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "4" then
+                randomString = ranFour[rand(ranFour.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "5" then
+                randomString = ranFive[rand(ranFive.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "6" then
+                randomString = ranSix[rand(ranSix.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "7" then
+                randomString = ranSev[rand(ranSev.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "8" then
+                randomString = ranEig[rand(ranEig.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "9" then
+                randomString = ranNin[rand(ranNin.length)]
+                 myString[i] = randomString
+              elsif myString[i] == "0" then
+                randomString = ranZero[rand(ranZero.length)]
+                 myString[i] = randomString       
+              else 
+              end
+        else  
+         
+        end
+        # increments i by 1
+      i = 1+i
+      
+      end
+length = myString.length
+puts length
+   
+
+return myString
+
+	end
 
 	def generateletterpass(genlength)
 	i = 0
