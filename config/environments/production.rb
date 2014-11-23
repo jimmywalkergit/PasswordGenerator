ce73247c3e4b2555d8f1a1ed63a341d92f8e4d1a
jimmywalkergit <jimmy.the.walk@gmail.com>
2014-11-22 21:54:17 -0500

    updated production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ PasswordGenerator::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.assets.precompile += ['bootstrap.scss']
-  config.assets.precompile += ['form_style.scss']
+  config.assets.precompile += ['bootstrap.css.scss']
+  config.assets.precompile += ['form_style.css.scss']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -23,14 +23,14 @@ PasswordGenerator::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
